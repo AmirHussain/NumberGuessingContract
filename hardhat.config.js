15839de2e51619d5a13f164968aa8699706f710e
=amir.hussain <=amir.hussain@10pearls.com>
2022-09-14 18:59:57 +0500

    setup localhost network for deploying

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const alchemy_api_key='uPmjCLS1ipphHFXTKDctQN8GYoQ1wCSR'
 /** @type import('hardhat/config').HardhatUserConfig */
+const GOERLI_PRIVATE_KEY = "e206df84d255451dae5ae8dd8efb3674";
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      account:['0xFE029208b267daBF5077bB3E3E7B1cc9916e9943']
+    },
+    hardhat:{
+      chainId: 1337
+    },
+    goerli:{
+        url: `https://eth-goerli.g.alchemy.com/v2/uPmjCLS1ipphHFXTKDctQN8GYoQ1wCSR`,
+        chainId: 5
+
+    }
+  }
 };
