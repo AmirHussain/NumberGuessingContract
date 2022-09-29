f34081299433b46df335e29e402536186518103f
=amir.hussain <=amir.hussain@10pearls.com>
2022-09-29 22:13:25 +0500

    add governance token

--- contracts/Governance.sol
+++ contracts/Governance.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
-pragma solidity ^0.8.9;
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol';
 import '@openzeppelin/contracts/utils/math/SafeMath.sol';
