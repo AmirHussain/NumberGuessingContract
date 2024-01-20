2486c6ea1263283cea84911f3ca994643139c0f7
Amir Hussain <amirsamtio@gmail.com>
2024-01-20 14:50:34 +0500

    update deployment scripts

--- scripts/deployNumberGuessingGame.js
+++ scripts/deployNumberGuessingGame.js
@@ -1,40 +1,40 @@
 
-const {ethers,network} = require("hardhat");
+const { ethers, network } = require("hardhat");
 
 async function main() {
-  if (network.name != "obscuro")  {
+  if (network.name != "obscuro") {
     console.warn("This needs to be on obscuro");
     process.exit(1)
-}
-const [owner] = await ethers.getSigners();
-console.log(network.name);
-console.log("owner",await owner.getAddress());
-await deployStuff();
+  }
+  const [owner] = await ethers.getSigners();
+  console.log(network.name);
+  console.log("owner", await owner.getAddress());
+  await deployStuff();
 }
 
 async function deployStuff() {
   const [deployer] = await ethers.getSigners();
-  console.log("deployer : ",deployer.address)
+  console.log("deployer : ", deployer.address)
 
-  customERC20Address = 0
-  
-  let CUSTOM_ERC20 = await ethers.getContractFactory("customERC20")
+  NumberGuessingGameAddress = 0
 
-  if (customERC20Address == 0) {
-      console.log("deploy customERC20 on obscuro")
-      customERC20 = await CUSTOM_ERC20.deploy('NUOGUESS', 'NGT')
-     
-      console.log("customERC20 contract : ",customERC20.address," tx ",customERC20.deployTransaction.hash)
-      console.log("deployed customERC20 : ",customERC20.deployTransaction.hash, customERC20.address)
-      recipt = await customERC20.deployTransaction.wait(2)
-      console.log(recipt.status == 1 ? "success": "failed")
-      await run("verify:verify", {
-          address: customERC20.address,
-          constructorArguments: ['NUOGUESS', 'NGT']
-      });
+  let NumberGuessingGame = await ethers.getContractFactory("NumberGuessingGame")
+
+  if (NumberGuessingGameAddress == 0) {
+    console.log("deploy NumberGuessingGame on obscuro")
+    NumberGuessingGame = await NumberGuessingGame.deploy('0x02e1D4BA29c46dC8153548C4bC37a6594F8eaEc1')
+
+    console.log("NumberGuessingGame contract : ", NumberGuessingGame.address, " tx ", NumberGuessingGame.deployTransaction.hash)
+    console.log("deployed NumberGuessingGame : ", NumberGuessingGame.deployTransaction.hash, NumberGuessingGame.address)
+    recipt = await NumberGuessingGame.deployTransaction.wait(2)
+    console.log(recipt.status == 1 ? "success" : "failed")
+    await run("verify:verify", {
+      address: NumberGuessingGame.address,
+      constructorArguments: ['0x02e1D4BA29c46dC8153548C4bC37a6594F8eaEc1']
+    });
 
   } else {
-      customERC20 = await customERC20.attach(customERC20Address)
+    NumberGuessingGame = await NumberGuessingGame.attach(NumberGuessingGameAddress)
   }
 
 }
